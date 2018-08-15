e06ab61117d125f7b49fd2ae0a40a59b9f5bafdd
lenovo <lenovo@192.168.0.139>
2018-08-15 20:05:25 +0800

    in1

---
+++ src/main/java/a.java
@@ -0,0 +1,19 @@
+package default_package;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import entity.*;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class test {
+ public static void main(String[] args) {
+	           System.out.println("aaa");
+	          //613249852684564
+}
+}
